02039318586a16e601a42ab4cc029f1b7c635758
Anton Sedin <bukleting@yandex.ru>
2021-12-29 19:54:09 +0700

    bugfix: fix svg inline mode and search null result

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,6 @@ import React from 'react';
 import './Header.css';
 
 // Импорт изображений
-import mailSvg from '../../images/mail.svg';
-import calendarSvg from '../../images/calendar.svg';
-import starSvg from '../../images/star.svg';
-import bellSvg from '../../images/bell.svg';
-import searchSvg from '../../images/search.svg';
 import profileImg from '../../images/avatar.jpg';
 
 function Header() {
@@ -18,25 +13,45 @@ function Header() {
         <ul className="header-navlist">
           <li className="header-navlist__item">
             <a href="/" className="header-navlist__link" title="Перейти на страницу сообщений">
-              <img src={mailSvg} alt="Сообщения" className="header-navlist__img" />
+              <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M3.10282 1H19.9254C21.082 1 22.0282 1.9 22.0282 3V15C22.0282 16.1 21.082 17 19.9254 17H3.10282C1.94627 17 1 16.1 1 15V3C1 1.9 1.94627 1 3.10282 1Z" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M22.0282 3L11.5141 10L1 3" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
             </a>
           </li>
           <li className="header-navlist__item">
             <a href="/" className="header-navlist__link" title="Перейти на страницу календаря">
-              <img src={calendarSvg} alt="Календарь" className="header-navlist__img" />
+              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="1.15918" y="3" width="18.9254" height="18" rx="2" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M14.8275 1V5" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M6.41621 1V5" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M1.15918 9H20.0845" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
             </a>
           </li>
           <li className="header-navlist__item">
             <a href="/" className="header-navlist__link" title="Перейти на страницу избранного">
-              <img src={starSvg} alt="Избранное" className="header-navlist__img" />
+              <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M11.7297 1L14.9785 7.26L22.2438 8.27L16.9867 13.14L18.2274 20.02L11.7297 16.77L5.23196 20.02L6.47262 13.14L1.21558 8.27L8.48081 7.26L11.7297 1V1Z" stroke="#FFA84C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
             </a>
           </li>
         </ul>
       </div>
       <div className="header__right-part">
         <div className="header-buttonlist">
-          <button className="header-button header-buttonlist__item" aria-label="Оповещения"><img src={bellSvg} alt="Оповещения" /></button>
-          <button className="header-button header-buttonlist__item" aria-label="Поиск"><img src={searchSvg} alt="Поиск" /></button>
+          <button className="header-button header-buttonlist__item" aria-label="Оповещения">
+            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17.3792 7C17.3792 3.68629 14.5548 1 11.0707 1C7.58667 1 4.76227 3.68629 4.76227 7C4.76227 14 1.60803 16 1.60803 16H20.5335C20.5335 16 17.3792 14 17.3792 7" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M12.8896 20C12.5134 20.6168 11.8204 20.9965 11.0707 20.9965C10.321 20.9965 9.62806 20.6168 9.25189 20" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
+          <button className="header-button header-buttonlist__item" aria-label="Поиск">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" clipRule="evenodd" d="M10.0758 17C14.7212 17 18.4871 13.4183 18.4871 9C18.4871 4.58172 14.7212 1 10.0758 1C5.4304 1 1.66455 4.58172 1.66455 9C1.66455 13.4183 5.4304 17 10.0758 17Z" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M20.5898 19L16.0162 14.65" stroke="#606060" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
         </div>
         <a href="/" className="header__profile-link" title="John Doe">
           <div className="header__profile-wrapper">
--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -1,10 +1,5 @@
 import React from 'react'
 
-// Импорт изображений
-import productStarSvg from '../../images/product-star.svg';
-import productHeartSvg from '../../images/heart.svg';
-import productBagSvg from '../../images/shopping-bag.svg';
-
 // Импорт стилей
 import './ProductGridItem.css';
 
@@ -16,18 +11,30 @@ function Product({ image, rating, price, title, desc, id, brandId, categoryId })
       </div>
       <div className="product__info">
         <div className="product__wrapper">
-            <button className="product__rating-btn">
-              <span className="product__rating-number">{rating}</span>
-              <img className="product__rating-star" src={productStarSvg} alt={rating} />
-            </button>
+          <button className="product__rating-btn">
+            <span className="product__rating-number">{rating}</span>
+            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" clipRule="evenodd" d="M7.69751 1L9.64065 4.94953L13.986 5.58675L10.8418 8.65931L11.5838 13L7.69751 10.9495L3.81123 13L4.55327 8.65931L1.40903 5.58675L5.75437 4.94953L7.69751 1V1Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
           <span className="product__price">${price}</span>
         </div>
         <h2 className="product__title">{title}</h2>
         <p className="product__description">{desc}</p>
       </div>
       <div className="product__buttons">
-        <button className="product__wishlist-btn"><img src={productHeartSvg} alt="В избранное" /> Wishlist</button>
-        <button className="product__addtocart-btn"><img src={productBagSvg} alt="В корзину" /> Add to cart</button>
+        <button className="product__wishlist-btn">
+          <svg width="17" height="15" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M14.7054 2.14929C13.9814 1.41343 12.9991 1 11.9748 1C10.9506 1 9.96828 1.41343 9.2442 2.14929L8.50012 2.90512L7.75604 2.14929C6.24796 0.61741 3.80287 0.617411 2.29479 2.14929C0.786706 3.68118 0.786706 6.16485 2.29479 7.69674L3.03887 8.45256L8.50012 14L13.9614 8.45256L14.7054 7.69674C15.4299 6.96122 15.8369 5.96344 15.8369 4.92302C15.8369 3.88259 15.4299 2.88481 14.7054 2.14929Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          Wishlist</button>
+        <button className="product__addtocart-btn">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.737212" fillRule="evenodd" clipRule="evenodd" d="M4.03651 1L1.24162 4.6V17.2C1.24162 18.1941 2.07583 19 3.10488 19H16.1477C17.1767 19 18.011 18.1941 18.011 17.2V4.6L15.2161 1H4.03651Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <path opacity="0.737212" d="M1.24162 5.5H18.011" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <path opacity="0.737212" d="M13.8186 9C13.8186 10.6569 12.1763 12 10.1503 12C8.12439 12 6.48204 10.6569 6.48204 9" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          Add to cart</button>
       </div>
     </li>
   )
